e0d64d65f40d4d9a25e90c1299e708e0d6d699ae
jaspur <995594631@qq.com>
2021-04-12 16:28:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Main/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.avjindersinghsekhon.minimaltodo.Main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.avjindersinghsekhon.minimaltodo.About.AboutActivity;
+import com.example.avjindersinghsekhon.minimaltodo.AppDefault.AppDefaultActivity;
+import com.example.avjindersinghsekhon.minimaltodo.R;
+import com.example.avjindersinghsekhon.minimaltodo.Settings.SettingsActivity;
+
+public class MainActivity extends AppDefaultActivity {
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+    }
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(false);
+//        }
+        @Override
+        public void onBackPressed() {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
+
+
+    @Override
+    protected int contentViewLayoutRes() {
+        return R.layout.activity_main;
+    }
+
+    @NonNull
+    @Override
+    protected Fragment createInitialFragment() {
+        return MainFragment.newInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.aboutMeMenuItem:
+                Intent i = new Intent(this, AboutActivity.class);
+                startActivity(i);
+                return true;
+//            case R.id.switch_themes:
+//                if(mTheme == R.style.CustomStyle_DarkTheme){
+//                    addThemeToSharedPreferences(LIGHTTHEME);
+//                }
+//                else{
+//                    addThemeToSharedPreferences(DARKTHEME);
+//                }
+//
+////                if(mTheme == R.style.CustomStyle_DarkTheme){
+////                    mTheme = R.style.CustomStyle_LightTheme;
+////                }
+////                else{
+////                    mTheme = R.style.CustomStyle_DarkTheme;
+////                }
+//                this.recreate();
+//                return true;
+            case R.id.preferences:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
+
+
